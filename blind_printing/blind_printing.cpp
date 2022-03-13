4dd5e214fe226bcf2ec013d14622e1939954e71a
Kulikova-A18 <81427431+Kulikova-A18@users.noreply.github.com>
2022-03-13 05:30:49 +0300

    Add files via upload

---
+++ blind_printing/blind_printing.cpp
@@ -0,0 +1,66 @@
+﻿#define _CRT_SECURE_NO_WARNINS
+
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+#include <stdio.h>
+#include <cstdio>
+#include <cstring>
+#include <string.h>
+#include <wchar.h>
+#include <ctime>
+#include <clocale>
+
+#include "print.h"
+
+using namespace std;
+
+char a;
+
+bool _bool = false;
+
+int main()
+{
+	srand((unsigned int)time(0));
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	size_t length = sizeof(data_print) / sizeof(*data_print);
+	for (int i = 0; i < length; i++) {
+		int random = rand() % length + 1;
+
+		char* char_arr;
+		string str_obj(data_print[random]);
+		char_arr = &str_obj[0];
+		cout << char_arr << endl;
+
+		for (int k = 0; k < str_obj.length(); k++) {
+			while (_bool == false) {
+
+				char _a = '\0';
+				while (_a == '\0') {
+					if (_kbhit()) {
+						a = _getch();
+						if (a == char_arr[k]) {
+							cout << a;
+						}
+						else {
+							SetConsoleTextAttribute(hConsole, 12);
+							cout << a;
+						}
+						_a = a;
+						a = '\0';
+					}
+					SetConsoleTextAttribute(hConsole, 7);
+				}
+				_bool = !_bool;
+				_a = '\0';
+			}
+			_bool = false;
+		}
+		system("cls");
+	}
+	return 0;
+}
---
+++ blind_printing/print.h
@@ -0,0 +1,108 @@
+#pragma once
+
+const char* data_print[] = {
+	"Однажды", "пингвины", "средь", "белых", "торосов",
+	"В", "которых", "пингвиний", "находится", "дом",
+	"Нашли", "погребённый", "под", "снежным", "заносом",
+	"Каким-то матросом оброненный том",
+
+	"И стали", "читать", "его", "попеременно",
+	"С большим выражением читали", "и вслух",
+	"И стало нежданным для них озареньем",
+	"Что", "птицы", "к", "зиме", "улетают", "на", "юг",
+
+	"Ну надо же", "охали братья", "пингвины",
+	"Какой сокрушительный", "инфо", "удар" , "!",
+	"А мы", "то зимою", "холодные", "льдины",
+	"Собой согреваем", "И облачком пар",
+
+	"На дружною стаей пингвиньей поднялся",
+	"И так же внезапно", "развеялся вдруг",
+	"Когда", "торжествующий", "рокот", "раздался",
+	"Ну всё!", "Решено!", "Улетаем на юг!",
+
+	"Вот", "собраны", "в", "кучу", "пингвиньи", "пожитки",
+	"По", "камню", "на птицу", "яйцо", "птенец",
+	"Короткие", "крылья", "захлопали", "прытко",
+	"И стук участился", "пингвиньих", "сердец",
+
+	"Но что это?", "Леность и сытые годы",
+	"Немного", "сместили", "для них", "ориентир",
+	"Решили не ждать", "в океане", "погоды",
+	"А скинуть", "полёту мешающий жир!",
+
+	"Так", "сказано-сделано", "ходят по кругу",
+	"Пингвины три года и месяц подряд",
+	"И дружно массируют спинки", "друг другу",
+	"Но только", "как прежде", "увы", "не летят",
+
+	"Но не покидает пингвинов стремление",
+	"И утром осенним", "когда-нибудь", "вдруг",
+	"Построятся клином", "они", "без сомнения",
+	"И без промедления умчатся на юг",
+
+	"И если вы встретите где-то пингвина",
+	"Погладьте его", "по спине и хвосту",
+	"Скажите пингвину", "что он молодчина",
+	"Но только не рушьте пингвинью мечту",
+
+	"Сложность текста", "очень легкий",
+
+	"Бедная старушка находит на дороге горшок с золотом", "Но пока она тащит его домой",
+	"золото превращается с серебро", "серебро", "в железо и так далее", "Но старушка рада",
+	"любой", "находке", "не теряет оптимизма и хорошего настроения",
+
+	"Достоинства рассказа", "Достоинства рассказа : Старушка из этой сказки ­– один",
+	"из самых позитивных персонажей", "английской", "Она воспринимает любые",
+	"перемены как подарок и учит нас видеть не сиюминутные последствия событий",
+	"их далекие перспективы", 
+
+	"There was once a woman who was very", "very cheerful", "though she had little to",
+	"make her so", "for she was old,and poor,and lonely", "She lived in a little bit",
+	"of a cottageand earned a scant", "living by running errands for her neighbours",
+	"getting a bite here, a sup there, as reward for her services", "So she made",
+	"shift to get on", "and", "always", "looked as ", "cheery as if she had", 
+	"not a want in the world",
+
+	"Now one summer evening", "as", "she was", "trotting", "full of smiles as ever", "along",
+	"the high road to her hovel", "what should she", "see but a big black pot lying in the ditch!",
+
+	"Goodness me!", "she cried", "that would", "be just the", "very thing", "for me if",
+	"only had", "something", "to put in it", "But", "I haven't!", "Now who could have",
+	"left it in the ditch?",
+
+	"And she looked about her expecting", "the owner", "would", "not be", "far off", 
+	"but she could see nobody",
+
+	"Maybe there is a hole in it", "she went on", "and that's", "why it has been cast away",
+	"But it would do fine to put", "a flower in for my window", "so I'll just take it home with me",
+
+	"And with that she", "lifted the lid and looked inside", "Mercy me!", "she cried",
+	"fair amazed", "If it isn't", "full of gold pieces. Here's luck!",
+
+	"And so it was", "brimful of great gold coins", "Well, at first she simply",
+	"stood stock-still", "wondering if she was standing on her head or her heels",
+	"Then she began saying",
+
+	"Lawks! But I do feel rich. I feel awful rich!",
+
+	"After she had said this many times", "she began to wonder how she was",
+	"to get her treasure home", "It was too heavy for", "her to carry, and she",
+	"could see no better", "way than to", "tie the end of her shawl", "to", "it", "and",
+	"drag", "it behind her like a go-cart",
+
+	"It will soon be dark", "she said to herself as she trotted along", "So",
+	"much the better", "The neighbours", "will", "not see what", "I'm bringing home",
+	"and I shall", "have all", "the night to myself", "and be able to think what",
+	"I'll do!", "Mayhap", "I'll", "buy a grand house", "and just sit by the fire with",
+	"a cup o", "tea and do no work at all like a queen", "Or maybe I'll bury",
+	"it at the garden", "foot and just keep", "a bit in the old china", "teapot on",
+	"the chimney-piece", "Or maybe—Goody", "Goody!", "I feel", "that grand", "I don't",
+	"know myself", "And she looked about her expecting the owner would not",
+	"be far off", "but she could see nobody",
+
+	"Maybe there is a hole in it", "she went on", "and that's why it has been",
+	"cast away", "But it would do fine to put a flower in for my window",
+	"so just take it home with me"
+
+	};
